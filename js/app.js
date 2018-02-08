02c111aa81c08f74ce38459c7fac3691aeb6a13d
LACY HOGAN <lacy.hogan@gmail.com>
2018-02-08 10:00:10 -0800

    Updated score in js.

--- js/app.js
+++ js/app.js
@@ -13,11 +13,9 @@ if (answer1 === 'yes'||answer1 === 'y') {
   alert('Right ' + userName + ', I have one adopted dog Hazel!');
   score++;
 } else if (answer1 === 'no' || answer1 === 'n') {
-  alert('Sorry ' + userName + ', that is wrong. I love dogs. In fact I own a rescue named Hazel.');
-  score = 0;
+  alert('Sorry ' + userName + ', that is wrong. I love dogs. In fact I own a rescue named Hazel.')
 } else {
-  alert('Sorry, you did not enter a valid answer. Perhaps you made a typo. We will move on, but yes I love dogs.');
-  score = 0;
+  alert('Sorry, you did not enter a valid answer. Perhaps you made a typo. We will move on, but yes I love dogs.')
 }
 
 var answer2 = prompt('Next question! Do I have siblings?').toLowerCase().trim();
@@ -27,11 +25,9 @@ if (answer2 === 'yes' || answer2 === 'y'){
   alert('That is correct ' + userName + '! I have 3 sisters and 3 brothers.');
   score++;
 } else if (answer1 === 'no' || answer1 === 'n') {
-  alert('That is incorrect ' + userName + ', I have 6 siblings! 3 brother and 3 sisters.');
-  score = 0;
+  alert('That is incorrect ' + userName + ', I have 6 siblings! 3 brother and 3 sisters.')
 } else {
-  alert('Shucks, you did not enter a valid answer. We will move on, but I have 6 siblings.');
-  score = 0;
+  alert('Shucks, you did not enter a valid answer. We will move on, but I have 6 siblings.')
 }
 
 var answer3 = prompt('Third question! Do I have a boat?').toLowerCase().trim();
@@ -41,11 +37,9 @@ if (answer3 === 'no' || answer3 === 'n'){
   alert('Awesome job ' + userName + '! I do not own a boat, but I do like to go out sailing with my friends.');
   score++;
 } else if (answer3 === 'yes' || answer3 === 'y'){
-  alert('Uh oh ' + userName + ', you got this one wrong. I do not own one, but maybe I will one day.');
-  score = 0;
+  alert('Uh oh ' + userName + ', you got this one wrong. I do not own one, but maybe I will one day.')
 } else {
-  alert('It looks like you entered an invalid answer. I do not own a boat.');
-  score = 0;
+  alert('It looks like you entered an invalid answer. I do not own a boat.')
 }
 
 var answer4 = prompt('Next question! Do I like to travel to foriegn countries?').toLowerCase().trim();
@@ -55,11 +49,9 @@ if (answer4 === 'yes' || answer4 === 'y'){
   alert('Right on ' + userName + '! I have actualy been to over 15 countries. Some of them for pleasure and some due to work. My favorite country so far is Croatia!');
   score++;
 } else if (answer4 === 'no' || answer4 === 'n'){
-  alert('Nope, you got this one wrong ' + userName + ', I love traveling overseas! I have been to over 15 countries so far. In fact, I will be traveling to Japan next year!');
-  score = 0;
+  alert('Nope, you got this one wrong ' + userName + ', I love traveling overseas! I have been to over 15 countries so far. In fact, I will be traveling to Japan next year!')
 } else {
-  alert('Dang, you must have typed your answer wrong. I love to travel!');
-  score = 0;
+  alert('Dang, you must have typed your answer wrong. I love to travel!')
 }
 
 var answer5 = prompt('Fifth question! Do I have kids?').toLowerCase().trim();
@@ -69,11 +61,9 @@ if (answer5 === 'no' || answer5 === 'n'){
   alert('You rock ' + userName + ', I do not have any kids. Though I do call Hazel, my dog, my kid sometimes. Haha!');
   score++;
 } else if (answer5 === 'yes' || answer5 === 'y'){
-  alert('You got this one wrong ' + userName + ', I actually do not have any kids. Having a dog can feel like having a kid sometimes.');
-  score = 0;
+  alert('You got this one wrong ' + userName + ', I actually do not have any kids. Having a dog can feel like having a kid sometimes.')
 } else {
-  alert('Shoot, you must have not entered a proper response. I will give you the answer any way. I do not have any kids.');
-  score = 0;
+  alert('Shoot, you must have not entered a proper response. I will give you the answer any way. I do not have any kids.')
 }
 
 var x = 3;
@@ -88,12 +78,13 @@ while (turns > 0) {
     score++;
   } else if (answer6 < x) {
     alert(userName + ', you are too low.');
-    score = 0;
   } else {
     alert(userName + ', you are too high.');
-    score = 0;
   } 
   turns--
+  if (turns === 0){
+    alert('Sorry, you are out of guesses. It was 3');
+  }
 }
 
 var lacysStates = ['florida', 'california', 'north carolina', 'alabama', 'texas'];
@@ -112,12 +103,11 @@ while (turns > 0 && correct === false) {
   }
   if (correct === false) {
     alert(userName + ', sorry, I have not been to that state.');
-    score = 0
   }
   turns--
-    if (correct === true || turns === 0) {
+  if (correct === true || turns === 0) {
       alert('The states that I have visited besides Washington are: Florida, California, North Carolina, Alsabama, and Texas');
-    }
+  }
 }
 
 alert('You have answered ' + score + ' out of 7 questions correct!');
